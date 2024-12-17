bdd990d49794ba767925bc1610b2ef26116e6047
AbhishekSKini1317 <abhishek.kini@experionglobal.com>
2024-12-18 01:23:51 +0530

    api call using github

--- src/apiCall/apiCall.ts
+++ src/apiCall/apiCall.ts
@@ -1,6 +1,6 @@
 import { CovidDataItem } from "../types/type";
 
-const URL = "http://localhost:5000/covidData";
+const URL = "https://my-json-server.typicode.com/AbhishekSKini/dbjson/covidData";
 
 // Assuming getData is fetching an API and returning data of type { data: CovidDataItem[] }
 export const getData = async (): Promise<{ data: CovidDataItem[] }> => {
--- src/components/DataListing/DataListing.tsx
+++ src/components/DataListing/DataListing.tsx
@@ -1,28 +1,25 @@
-import React, { useEffect, useState } from 'react'
-import { getData } from '../../apiCall/apiCall'
-import CovidTable from './DataTable';
-import { useDispatch } from 'react-redux';
-import { setCovidDataAction } from '../../redux/covidDataSlice';
+import React, { useEffect, useState } from "react";
+import { getData } from "../../apiCall/apiCall";
+import CovidTable from "./DataTable";
+import { useDispatch } from "react-redux";
+import { setCovidDataAction } from "../../redux/covidDataSlice";
 import { CovidDataItem } from "../../types/type";
-
 
 const DataListing = () => {
   const [covidData, setCovidData] = useState<CovidDataItem[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string|null>(null);
+  const [error, setError] = useState<string | null>(null);
   const dispatch = useDispatch(); // Initialize Redux dispatch
   useEffect(() => {
     const fetchData = async () => {
       try {
         setIsLoading(true);
         const response = await getData();
-        console.log(response.data);
-        
         setCovidData(response.data);
         dispatch(setCovidDataAction(response.data)); // Dispatch the data to Redux store
       } catch (err) {
-        setError('Failed to fetch data');
-        console.error('Error fetching data:', err);
+        setError("Failed to fetch data");
+        console.error("Error fetching data:", err);
       } finally {
         setIsLoading(false);
       }
@@ -40,10 +37,10 @@ const DataListing = () => {
   }
 
   return (
-    <div >
+    <div>
       <CovidTable data={covidData} />
     </div>
   );
-}
+};
 
-export default DataListing
+export default DataListing;
